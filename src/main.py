d1580a78008168f19ab5bbd979ec300bb159f47b
Wings <wings.xiangyi.zeng@gmail.com>
2025-08-11 00:04:00 +0800

    style: Format code for improved readability
    Applied automatic code formatting to improve consistency and readability
    across the file. No functional changes were made.

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import pickle
 import platform
 import socket
 import time
-from typing import Dict, List, Any
+from typing import Any, Dict, List
 from urllib.parse import urlparse
 
 from loguru import logger
@@ -19,8 +19,19 @@ class ATrustLoginStorage(BaseModel):
     cookies: List[Dict[str, Any]]
     local_storage: Dict[str, Any]
 
+
 class ATrustLogin:
-    def __init__(self, portal_address, driver_path=None, browser_path=None, driver_type=None, data_dir="data", interactive=False, input_delay=0.5, loading_delay=5):
+    def __init__(
+        self,
+        portal_address,
+        driver_path=None,
+        browser_path=None,
+        driver_type=None,
+        data_dir="data",
+        interactive=False,
+        input_delay=0.5,
+        loading_delay=5,
+    ):
         self.initialized = False
         if not os.path.exists(data_dir):
             os.makedirs(data_dir, exist_ok=True)
@@ -30,8 +41,13 @@ class ATrustLogin:
         self.input_delay = input_delay
         self.loading_delay = loading_delay
 
-        self.must_be_logged_keywords = ['app_center', 'user_info', 'app_apply', 'device_manage']
-        self.must_not_logged_keywords = ['login', 'captcha']
+        self.must_be_logged_keywords = [
+            "app_center",
+            "user_info",
+            "app_apply",
+            "device_manage",
+        ]
+        self.must_not_logged_keywords = ["login", "captcha"]
 
         if driver_type is None:
             system = platform.system()
@@ -45,15 +61,15 @@ class ATrustLogin:
         if driver_type == "edge":
             from selenium.webdriver.edge.options import Options
             from selenium.webdriver.edge.service import Service
-        else :
-            from selenium.webdriver.chrome.service import Service
+        else:
             from selenium.webdriver.chrome.options import Options
+            from selenium.webdriver.chrome.service import Service
 
         # 配置Edge Driver选项
         self.options = Options()
 
         # self.options.add_argument(f'--user-data-dir="{data_dir}"')
-        self.options.add_argument(f'--profile-directory=ATrustLogin')
+        self.options.add_argument(f"--profile-directory=ATrustLogin")
         # options.add_argument("--start-maximized")
         self.options.add_argument("--ignore-certificate-errors")
         self.options.add_argument("--ignore-ssl-errors")
@@ -63,7 +79,9 @@ class ATrustLogin:
         self.options.add_argument("--disable-extensions")
         self.options.add_argument("--window-size=896,672")
 
-        self.options.add_experimental_option("prefs", {"intl.accept_languages": "zh-CN"})
+        self.options.add_experimental_option(
+            "prefs", {"intl.accept_languages": "zh-CN"}
+        )
 
         if browser_path is not None:
             self.options.binary_location = browser_path
@@ -73,7 +91,7 @@ class ATrustLogin:
 
         if driver_type == "edge":
             self.driver = webdriver.Edge(service=service, options=self.options)
-        else :
+        else:
             self.driver = webdriver.Chrome(service=service, options=self.options)
 
         self.wait = WebDriverWait(self.driver, 10)
@@ -83,7 +101,9 @@ class ATrustLogin:
         self.driver.get(self.portal_address)
 
     def wait_login_page(self):
-        self.wait.until(lambda d: d.execute_script("return document.readyState") == "complete")
+        self.wait.until(
+            lambda d: d.execute_script("return document.readyState") == "complete"
+        )
 
     def delay_input(self):
         time.sleep(self.input_delay)
@@ -94,7 +114,10 @@ class ATrustLogin:
     # 输入用户名和密码
     def enter_credentials(self, username, password):
         try:
-            element = self.driver.find_element(By.XPATH, "//div[contains(@class, 'server-name') and contains(text(), '本地密码')]")
+            element = self.driver.find_element(
+                By.XPATH,
+                "//div[contains(@class, 'server-name') and contains(text(), '本地密码')]",
+            )
             if element.is_displayed():
                 self.delay_input()
                 self.scroll_and_click(element)
@@ -128,17 +151,21 @@ class ATrustLogin:
         # 从pickle文件中加载存储的数据
         try:
             if os.path.exists(os.path.join(self.data_dir, "ATrustLoginStorage.pkl")):
-                with open(os.path.join(self.data_dir, "ATrustLoginStorage.pkl"), "rb") as f:
+                with open(
+                    os.path.join(self.data_dir, "ATrustLoginStorage.pkl"), "rb"
+                ) as f:
                     data = pickle.load(f)
                     # 从cookies中加载cookie
                     for cookie in data.cookies:
-                        self.driver.delete_cookie(cookie['name'])
+                        self.driver.delete_cookie(cookie["name"])
                         self.driver.add_cookie(cookie)
                     # 从local_storage中加载local storage
                     for key, value in data.local_storage.items():
                         key_js = json.dumps(key)
                         value_js = json.dumps(value)
-                        self.driver.execute_script(f"window.localStorage.setItem({key_js}, {value_js});")
+                        self.driver.execute_script(
+                            f"window.localStorage.setItem({key_js}, {value_js});"
+                        )
                     logger.info("Loaded storage data")
         except FileNotFoundError:
             logger.info("未找到存储的数据")
@@ -191,7 +218,7 @@ class ATrustLogin:
         :return: None if not sure, True if logged, False if not logged
         """
 
-        if self.driver.current_url.startswith('about:'):
+        if self.driver.current_url.startswith("about:"):
             return None
 
         url = urlparse(self.driver.current_url)
@@ -201,7 +228,10 @@ class ATrustLogin:
         if any(keyword in url.fragment for keyword in self.must_not_logged_keywords):
             return False
 
-        return "工作台" in self.driver.page_source and "本地密码" not in self.driver.page_source
+        return (
+            "工作台" in self.driver.page_source
+            and "本地密码" not in self.driver.page_source
+        )
 
     def close(self):
         self.driver.quit()
@@ -212,7 +242,7 @@ class ATrustLogin:
     def update_storage(self):
         data = ATrustLoginStorage(
             cookies=self.driver.get_cookies(),
-            local_storage=self.driver.execute_script("return window.localStorage")
+            local_storage=self.driver.execute_script("return window.localStorage"),
         )
 
         # save with pickle
@@ -223,7 +253,7 @@ class ATrustLogin:
         self.close()
 
     @staticmethod
-    def wait_for_port(port, host='localhost', loading_delay=5):
+    def wait_for_port(port, host="localhost", loading_delay=5):
         while True:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 s.settimeout(1)
@@ -233,17 +263,42 @@ class ATrustLogin:
                     s.close()
                     break
                 except (socket.timeout, ConnectionRefusedError):
-                    logger.info(f"aTrust Port {port} is not yet being listened on. Waiting for aTrust start ...")
+                    logger.info(
+                        f"aTrust Port {port} is not yet being listened on. Waiting for aTrust start ..."
+                    )
                     time.sleep(loading_delay)
 
-def main(username, password, portal_address="https://passport.escience.cn/oauth2/authorize?theme=arp_2018&client_id=59145&redirect_uri=https%3A%2F%2F159.226.243.221%3A443%2Fpassport%2Fv1%2Fauth%2FhttpsOauth2%3FsfDomain%3DOAuth&response_type=code", keepalive=200, data_dir="./data", driver_type=None, driver_path=None, browser_path=None, interactive=False, wait_atrust=True, input_delay=0.5, loading_delay=5):
+
+def main(
+    username,
+    password,
+    portal_address="https://passport.escience.cn/oauth2/authorize?theme=arp_2018&client_id=59145&redirect_uri=https%3A%2F%2F159.226.243.221%3A443%2Fpassport%2Fv1%2Fauth%2FhttpsOauth2%3FsfDomain%3DOAuth&response_type=code",
+    keepalive=200,
+    data_dir="./data",
+    driver_type=None,
+    driver_path=None,
+    browser_path=None,
+    interactive=False,
+    wait_atrust=True,
+    input_delay=0.5,
+    loading_delay=5,
+):
     logger.info("Opening Web Browser")
 
     if wait_atrust:
         ATrustLogin.wait_for_port(54631, loading_delay=loading_delay)
 
     # 创建ATrustLogin对象
-    at = ATrustLogin(data_dir=data_dir, portal_address=portal_address, driver_type=driver_type, driver_path=driver_path, browser_path=browser_path, interactive=interactive, input_delay=input_delay, loading_delay=loading_delay)
+    at = ATrustLogin(
+        data_dir=data_dir,
+        portal_address=portal_address,
+        driver_type=driver_type,
+        driver_path=driver_path,
+        browser_path=browser_path,
+        interactive=interactive,
+        input_delay=input_delay,
+        loading_delay=loading_delay,
+    )
 
     at.init()
 
@@ -269,6 +324,8 @@ def main(username, password, portal_address="https://passport.escience.cn/oauth2
             logger.exception(e)
             at.delay_loading()
 
+
 if __name__ == "__main__":
     from fire import Fire
+
     Fire(main)
